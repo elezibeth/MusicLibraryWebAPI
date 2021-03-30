460a915852676fd76b045ca78a7a07afcc27bb3f
elezibeth <elezibethmorgan@my.unt.edu>
2021-03-30 14:00:41 -0700

    DbContext and song controller added and configured.

---
+++ MusicLibraryWebApi/Controllers/SongController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicLibraryWebApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicLibraryWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public SongController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<SongController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SongController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SongController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SongController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SongController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- MusicLibraryWebApi/Data/ApplicationDbContext.cs
+++ MusicLibraryWebApi/Data/ApplicationDbContext.cs
@@ -3,14 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MusicLibraryWebApi;
+using MusicLibraryWebApi.Models;
+using MusicLibraryWebApi.Data;
 
 namespace MusicLibraryWebApi.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions options)
-            : base(options);
+        protected ApplicationDbContext(DbContextOptions options)
+            : base(options)
         {
         }
+       
+        public DbSet<Song> Songs { get; set; }
+
     }
 }
